e2ee6705f0a08223e2da5b58b377ecdbcc8aa0fe
Michael Tirona <tironamichael@gmail.com>
2020-04-28 10:09:41 -0700

    Started fleshing out the array

---
+++ script.js
@@ -0,0 +1,23 @@
+const q1 = 'What show does the term \"hotdog\" come from?'
+const q2 = 'Temperate Literals use:'
+const q3 = 'A Boolean is a True/False statement'
+const q4 = 'What styling library can be a coder\'s best friend?'
+
+const choice1 = [
+    {
+        question: 'a) The Big Bang Theory',
+        isTrue: false
+    },
+    {
+        question: 'b) Silicon Valley',
+        isTrue: true,
+    }
+    {
+        question: 'c) Mr. Robot',
+        isTrue: false 
+    },
+    {
+        question: 'Freaks & Geeks',
+        isTrue: false
+    }
+]
