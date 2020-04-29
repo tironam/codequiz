e90f68a805a00682f064c7695d8b5f97c879ab76
Michael Tirona <tironamichael@gmail.com>
2020-04-28 23:11:00 -0700

    Added lots of HTML and JS

--- script.js
+++ script.js
@@ -1,63 +1,38 @@
-const q1 = 'What show does the term \"hotdog\" come from?'
-const q2 = 'Temperate Literals use:'
-const q3 = 'A Boolean is a True/False statement'
-const q4 = 'What styling library can be a coder\'s best friend?'
-const q5 = 'Making frequent commits is often a bad idea'
-const q6 = 'W3 Schools is a:'
-const q7 = 'It\'s best practice to use ____ for CSS'
-const q8 = 'It\'s best practice to use ___ for JavaScript'
-const q9 = 'The best browser for a web developer to use is:'
-const q10 = 'A great place to store your code repositories is:'
+const startButton = document.getElementById('startBtn')
+const qBox = document.getElementById('questionBox')
+let questionIndex
+const qElement = document.getElementById('question')
+const answerButton = document.getElementById('answerBtn')
 
-const choice1 = [
-    {
-        question: 'a) The Big Bang Theory',
-        isTrue: false
-    },
-    {
-        question: 'b) Silicon Valley',
-        isTrue: true
-    },
-    {
-        question: 'c) Mr. Robot',
-        isTrue: false
-    },
-    {
-        question: 'd) Freaks & Geeks',
-        isTrue: false
-    }
-]
+startButton.addEventListener('click', startGame)
 
-let correct = 0
-let incorrect = 0
-let question = 'none'
-let input = 'none'
-let answer = 'none'
+function startGame() {
+    startButton.classList.add('hide')
+    qBox.classList.remove('hide')
+    questionIndex = 0
+    nextQ()
+}
 
-let start = quizStart()
-
-document.getElementById('question').innerHTML = q1
-// document.getElementsById('choices').innerHTML = choice1.question
-
-for (let i = 0; i < choice1.length; i++) {
-    let btnElem = document.createElement('button')
-    btnElem.className = 'question'
-    btnElem.textContent = choice1[i].question
-    btnElem.dataset.index = i
-    document.getElementById('choices').append(btnElem)
+function nextQ () {
+    showQ(questionIndex)
 } 
 
-document.addEventListener('click', (event) => {
-    if (choice1) {
-        console.log('You got it right!')
-    } else {
-        console.log('You got it wrong :(')
-    }
-})
+function showQ(question) {
+    qElement.innerText = question.question
+}
 
-rightAnswer() {
+function choose() {
 
 }
-// // document.getElementById('choices').addEventListener('click', () => {
-// //     if 
-// // })
+
+const questions = [
+    {
+        question: 'Question 1',
+        answers: [
+            { text: 'no', correct: false }
+            { text: 'Yes', correct: true }
+            { text: 'narp', correct: false }
+            { text: 'nah', correct: false }
+        ]
+    }
+]
