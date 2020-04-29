3f35cf638c997bf95fec7c6be74151c559a18a23
Michael Tirona <tironamichael@gmail.com>
2020-04-28 23:58:37 -0700

    got start to work

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 const startButton = document.getElementById('startBtn')
-const qBox = document.getElementById('questionBox')
-let questionIndex
-const qElement = document.getElementById('question')
+const qBox = document.getElementById('answerBtn')
+let shuffledQ, questionIndex
+const qElement = document.getElementById('questionBox')
 const answerButton = document.getElementById('answerBtn')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    startButton.classList.add('hide')
-    qBox.classList.remove('hide')
+    startButton.classList.add('invisible')
+    shuffledQ = questions.sort(() => Math.random() - .5)
     questionIndex = 0
+    qBox.classList.remove('invisible')
     nextQ()
 }
 
 function nextQ () {
-    showQ(questionIndex)
+    // showQ(questionIndex)
 } 
 
-function showQ(question) {
-    qElement.innerText = question.question
-}
+// function showQ(questions) {
+//     qElement.innerText = questions.question
+// }
 
 function choose() {
 
@@ -29,10 +30,14 @@ const questions = [
     {
         question: 'Question 1',
         answers: [
-            { text: 'no', correct: false }
-            { text: 'Yes', correct: true }
-            { text: 'narp', correct: false }
-            { text: 'nah', correct: false }
+            {
+                text: 'no', 
+                correct: false
+            },
+            {
+             text: 'yes', 
+             correct: true
+            }
         ]
     }
 ]
