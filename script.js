1a3fb4b62b67e63496b746ca16bd6335f81f3ead
Michael Tirona <tironamichael@gmail.com>
2020-04-29 16:45:41 -0700

    Started work on button functions

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const startButton = document.getElementById('startBtn')
-const qBox = document.getElementById('questions')
+const qBox = document.getElementById('questContainer')
 const qElement = document.getElementById('questionBox')
 const answerButton = document.getElementById('answerBtn')
+const startTxt = document.getElementById('startTxt')
+const startBox = document.getElementById('startBox')
+let rightAnswer = document.getElementById('rightAnswer')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     startButton.classList.add('invisible')
+    startTxt.classList.add('invisible')
+    startBox.classList.add('byeBox')
     qBox.classList.remove('invisible')
 }
 
-
-
-function showQ(questions) {
-
+function chooseAnswer() {
+    document.getElementById.addEventListener('click', (event) => {
+        if (event.target.classList.contains('rightAnswer')) {
+            console.log('hey')
+        }
+    })
 }
 
 
